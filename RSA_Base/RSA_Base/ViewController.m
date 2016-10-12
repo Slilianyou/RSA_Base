c397b25ce5249fb1307f21d0e8097059d9968ec3
Slilianyou <li.lian.you163@163.com>
2016-10-12 10:12:03 +0800

    RSA利用公匙加密，用私匙解密

--- RSA_Base/RSA_Base/ViewController.m
+++ RSA_Base/RSA_Base/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+//#import <Security/Security.h>
+#import "RSAEncryptor.h"
 
 @interface ViewController ()
 
@@ -16,8 +18,56 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // 加密数据
+    RSAEncryptor * rsa = [[RSAEncryptor alloc]init];
+    NSLog(@"encryptor using rsa");
+    NSString *publicKeyPath = [[NSBundle mainBundle]pathForResource:@"public_key" ofType:@"der"];
+    NSLog(@"piblic key:%@",publicKeyPath);
+    
+    [rsa loadPublicKeyFromFile:publicKeyPath];
+    
+    NSString *securityText = @"hello ~";
+    NSString *encryptedString = [rsa rsaEncryptString:securityText];
+    NSLog(@"encrypted data:%@",encryptedString);
+    //L2ijuloVZ78m9TMFT6WyyZyAc2OihsApm4KF2Z6Z/U9gH7bkmiqLCUGlU4WY8VpukDV+HhGeXGFMMYv85MORPqvXG6DD4R9eOlJkWbBXwxM1UDy1cJsps1wNfIGXc5BTRwdWgh99ewpkznUw4rX1Fq/uV7x2NcUNxm+EGQn9VWU=
+    
+    // 解密数据
+    NSLog(@"decryptor using rsa");
+    [rsa loadPrivateKeyFromFile:[[NSBundle mainBundle]pathForResource:@"private_key" ofType:@"p12"] password:@"lly4486"];
+    NSString *decryptedString = [rsa rsaDecryptString:encryptedString];
+    NSLog(@"decrypted data: %@",decryptedString);
+     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 - (void)didReceiveMemoryWarning {
